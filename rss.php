d21781e03ac41c36f635fdb41afdeb0fceed5182
MichalZemcak85 <156913242+MichalZemcak85@users.noreply.github.com>
2025-09-23 23:09:35 +0200

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,30 +1,58 @@
 <?php
 header("Content-Type: application/rss+xml; charset=UTF-8");
 
-// Vytvoření základního XML
+// Načti HTML ze stránky SVJ
+$url = "https://kytlicka.eu/aktuality-a-oznameni/";
+$html = @file_get_contents($url);
+
+if ($html === false) {
+    die("Nelze načíst obsah stránky.");
+}
+
+libxml_use_internal_errors(true);
+$dom = new DOMDocument();
+$dom->loadHTML($html);
+$xpath = new DOMXPath($dom);
+
+// Najdi první článek
+$titleNode = $xpath->query("//h2")[0];  // Titulek článku
+$dateNode = $xpath->query("//p[contains(@class,'blog-date')]")[0]; // Datum
+$linkNode = $xpath->query("//h2/a")[0]; // Link
+
+$title = $titleNode ? trim($titleNode->nodeValue) : "Neznámý titulek";
+$link = $linkNode ? $linkNode->getAttribute("href") : "https://kytlicka.eu";
+if (strpos($link, "http") !== 0) {
+    $link = "https://kytlicka.eu" . $link; // Oprava relativního odkazu
+}
+$description = $title; // můžeš upravit nebo doplnit výpis úvodu článku
+
+// Zpracuj datum
+$dateText = $dateNode ? trim($dateNode->nodeValue) : date("d.m.Y");
+$timestamp = strtotime($dateText);
+$pubDate = date(DATE_RSS, $timestamp ?: time());
+
+// Vytvoření XML
 $xml = new DOMDocument("1.0", "UTF-8");
 $rss = $xml->createElement("rss");
 $rss->setAttribute("version", "2.0");
 $channel = $xml->createElement("channel");
 
-// Hlavní metadata
+// Metadata kanálu
 $channel->appendChild($xml->createElement("title", "SVJ Kytlická Novinky"));
-$channel->appendChild($xml->createElement("link", "https://kytlicka.eu/aktuality-a-oznameni/"));
+$channel->appendChild($xml->createElement("link", $url));
 $channel->appendChild($xml->createElement("description", "Novinky zveřejněné na webu SVJ Kytlická"));
 
-// Ukázková položka – sem můžeš doplnit dynamická data
+// Položka
 $item = $xml->createElement("item");
-$item->appendChild($xml->createElement("title", "Aktualizace k opravě střechy – blok A"));
-$item->appendChild($xml->createElement("link", "https://www.kytlicka.eu/l/aktualizace-k-oprave-strechy-blok-a/"));
-$item->appendChild($xml->createElement("description", "Aktualizace k opravě střechy – blok A"));
-$item->appendChild($xml->createElement("pubDate", date(DATE_RSS)));
-$item->appendChild($xml->createElement("guid", "https://www.kytlicka.eu/l/aktualizace-k-oprave-strechy-blok-a/"));
+$item->appendChild($xml->createElement("title", $title));
+$item->appendChild($xml->createElement("link", $link));
+$item->appendChild($xml->createElement("description", $description));
+$item->appendChild($xml->createElement("pubDate", $pubDate));
+$item->appendChild($xml->createElement("guid", $link));
 $channel->appendChild($item);
 
-// Uzavření XML
+// Finalizace
 $rss->appendChild($channel);
 $xml->appendChild($rss);
-
-// Výstup
 echo $xml->saveXML();
 ?>
