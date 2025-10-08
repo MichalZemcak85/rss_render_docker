4a0d51d0274b9118baf5ee0e58a81c6fe70b362a
MichalZemcak85 <156913242+MichalZemcak85@users.noreply.github.com>
2025-10-08 13:40:33 +0200

    Update rss.php

--- rss.php
+++ rss.php
@@ -15,51 +15,49 @@ $dom->loadHTML($html);
 $xpath = new DOMXPath($dom);
 
 // Najdi první článek
-$titleNode = $xpath->query("//h2")[0];  // Titulek článku
-$dateNode = $xpath->query("//p[contains(@class,'blog-date')]")[0]; // Datum
-$linkNode = $xpath->query("//h2/a")[0]; // Link
+$titleNode = $xpath->query("//h2")[0];       // Nadpis článku
+$linkNode  = $xpath->query("//h2/a")[0];     // Odkaz článku
+$dateNode  = $xpath->query("//em | //i | //p")[0]; // Najdi datum i bez class
 
 $title = $titleNode ? trim($titleNode->nodeValue) : "Neznámý titulek";
-$link = $linkNode ? $linkNode->getAttribute("href") : "https://kytlicka.eu";
+$link  = $linkNode ? $linkNode->getAttribute("href") : "https://kytlicka.eu";
 if (strpos($link, "http") !== 0) {
-    $link = "https://kytlicka.eu" . $link; // Oprava relativního odkazu
+    $link = "https://kytlicka.eu" . $link;
 }
-$description = $title; // můžeš doplnit úvod článku
+$description = $title;
 
 // 🧭 Zpracuj datum (formát 23.09.2025)
 $dateText = $dateNode ? trim($dateNode->nodeValue) : "";
-if (preg_match('/(\d{1,2})\.(\d{1,2})\.(\d{4})/', $dateText, $matches)) {
-    $day = (int)$matches[1];
-    $month = (int)$matches[2];
-    $year = (int)$matches[3];
+
+if (preg_match('/(\d{1,2})\.(\d{1,2})\.(\d{4})/', $dateText, $m)) {
+    $day = (int)$m[1];
+    $month = (int)$m[2];
+    $year = (int)$m[3];
     $timestamp = mktime(0, 0, 0, $month, $day, $year);
 } else {
-    // fallback pro jiné formáty
     $timestamp = strtotime($dateText);
 }
 
-// Pokud se datum nepodaří načíst, použij aktuální
 if (!$timestamp) {
-    $timestamp = time();
+    $timestamp = time(); // fallback
 }
 
 $pubDate = date(DATE_RSS, $timestamp);
 
-// Vytvoření XML
+// 🧱 Vytvoření XML
 $xml = new DOMDocument("1.0", "UTF-8");
 $xml->formatOutput = true;
 
 $rss = $xml->createElement("rss");
 $rss->setAttribute("version", "2.0");
-$channel = $xml->createElement("channel");
 
-// Metadata kanálu
+$channel = $xml->createElement("channel");
 $channel->appendChild($xml->createElement("title", "SVJ Kytlická Novinky"));
 $channel->appendChild($xml->createElement("link", $url));
 $channel->appendChild($xml->createElement("description", "Novinky zveřejněné na webu SVJ Kytlická"));
 $channel->appendChild($xml->createElement("lastBuildDate", $pubDate));
 
-// Položka článku
+// 📰 Jedna položka
 $item = $xml->createElement("item");
 $item->appendChild($xml->createElement("title", $title));
 $item->appendChild($xml->createElement("link", $link));
@@ -68,8 +66,9 @@ $item->appendChild($xml->createElement("pubDate", $pubDate));
 $item->appendChild($xml->createElement("guid", $link));
 $channel->appendChild($item);
 
-// Dokončení XML
 $rss->appendChild($channel);
 $xml->appendChild($rss);
+
+// Výstup XML
 echo $xml->saveXML();
 ?>
