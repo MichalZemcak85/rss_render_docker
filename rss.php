3160180bf19fb72998bc058feca28602bb5fa9d1
MichalZemcak85 <156913242+MichalZemcak85@users.noreply.github.com>
2025-11-08 23:22:58 +0100

    Update rss.php
    update 8.11

--- rss.php
+++ rss.php
@@ -1,44 +1,80 @@
 <?php
 header("Content-Type: application/rss+xml; charset=UTF-8");
 
-// Načti HTML ze stránky SVJ
-$url = "https://kytlicka.eu/aktuality-a-oznameni/";
-$html = @file_get_contents($url);
+$listUrl = "https://kytlicka.eu/aktuality-a-oznameni/";
 
-if ($html === false) {
-    die("Nelze načíst obsah stránky.");
+// 1) Načti seznam aktualit
+$listHtml = @file_get_contents($listUrl);
+if ($listHtml === false) {
+    die("Nelze načíst seznam aktualit.");
 }
 
-// Najdi datum ve formátu DD.MM.YYYY kdekoli v textu
-if (preg_match('/(\d{1,2})\.(\d{1,2})\.(\d{4})/', $html, $m)) {
-    $day = (int)$m[1];
-    $month = (int)$m[2];
-    $year = (int)$m[3];
-    $timestamp = mktime(0, 0, 0, $month, $day, $year);
-} else {
-    $timestamp = time(); // fallback, pokud datum není nalezeno
-}
-
-$pubDate = date(DATE_RSS, $timestamp);
-
-// Pokus o extrakci titulku a odkazu
 libxml_use_internal_errors(true);
-$dom = new DOMDocument();
-$dom->loadHTML($html);
-$xpath = new DOMXPath($dom);
+$listDom = new DOMDocument();
+$listDom->loadHTML($listHtml);
+$listXPath = new DOMXPath($listDom);
 
-$titleNode = $xpath->query("//h2")[0];
-$linkNode  = $xpath->query("//h2/a")[0];
+/**
+ * Najdeme první link, který vede na detail článku.
+ * Na Webnode bývají detaily ve tvaru ".../l/nejaky-clanek/".
+ */
+$linkNode = $listXPath->query("(//a[contains(@href, '/l/')])[1]")->item(0);
 
-$title = $titleNode ? trim($titleNode->nodeValue) : "Neznámý titulek";
-$link  = $linkNode ? $linkNode->getAttribute("href") : "https://kytlicka.eu";
-if (strpos($link, "http") !== 0) {
-    $link = "https://kytlicka.eu" . $link;
+if (!$linkNode) {
+    // Když nic nenajdeme, vrátíme prázdný, ale validní feed
+    $title = "Žádné aktuality nebyly nalezeny";
+    $link  = $listUrl;
+    $pubDate = date(DATE_RSS);
+} else {
+    // Sestav URL článku
+    $articleUrl = $linkNode->getAttribute("href");
+    if (strpos($articleUrl, "http") !== 0) {
+        $articleUrl = "https://kytlicka.eu" . $articleUrl;
+    }
+
+    // 2) Načti detail článku
+    $articleHtml = @file_get_contents($articleUrl);
+    if ($articleHtml === false) {
+        // fallback - kdyby selhalo načtení článku
+        $title = "Nelze načíst detail článku";
+        $link  = $articleUrl;
+        $pubDate = date(DATE_RSS);
+    } else {
+        $articleDom = new DOMDocument();
+        $articleDom->loadHTML($articleHtml);
+        $articleXPath = new DOMXPath($articleDom);
+
+        // Titulek: vezmeme první <h1>
+        $h1 = $articleXPath->query("//h1")->item(0);
+        $title = $h1 ? trim($h1->textContent) : "Aktualita SVJ Kytlická";
+
+        // Datum: zkusíme p s class obsahující 'date'
+        $dateNode = $articleXPath->query(
+            "//p[contains(@class,'date') or contains(@class,'blog-date')][1]"
+        )->item(0);
+
+        if ($dateNode) {
+            $dateText = trim($dateNode->textContent);  // např. "05.11.2025"
+            // sjednotit formát
+            $normalized = str_replace(' ', '', $dateText);
+            $normalized = str_replace('.', '-', $normalized);
+            $timestamp = strtotime($normalized);
+        } else {
+            $timestamp = false;
+        }
+
+        // Když se datum nepodaří přečíst, radši vezmeme dnešek (ale to by se stávat nemělo)
+        if ($timestamp === false || $timestamp <= 0) {
+            $timestamp = time();
+        }
+
+        $pubDate = date(DATE_RSS, $timestamp);
+        $link = $articleUrl;
+    }
 }
 
-$description = $title;
+// 3) Postavíme RSS XML s jedním itemem
 
-// Vytvoření XML
 $xml = new DOMDocument("1.0", "UTF-8");
 $xml->formatOutput = true;
 
@@ -47,19 +83,19 @@ $rss->setAttribute("version", "2.0");
 
 $channel = $xml->createElement("channel");
 $channel->appendChild($xml->createElement("title", "SVJ Kytlická Novinky"));
-$channel->appendChild($xml->createElement("link", $url));
+$channel->appendChild($xml->createElement("link", $listUrl));
 $channel->appendChild($xml->createElement("description", "Novinky zveřejněné na webu SVJ Kytlická"));
 $channel->appendChild($xml->createElement("lastBuildDate", $pubDate));
 
 $item = $xml->createElement("item");
 $item->appendChild($xml->createElement("title", $title));
 $item->appendChild($xml->createElement("link", $link));
-$item->appendChild($xml->createElement("description", $description));
+$item->appendChild($xml->createElement("description", $title));
 $item->appendChild($xml->createElement("pubDate", $pubDate));
 $item->appendChild($xml->createElement("guid", $link));
-$channel->appendChild($item);
 
+$channel->appendChild($item);
 $rss->appendChild($channel);
 $xml->appendChild($rss);
+
 echo $xml->saveXML();
-?>
